cb94a711f213514333dbea30ad44fbf600ec1380
--global <--global>
2024-10-06 09:43:10 +0000

    code

--- aqua_profile.plugin.zsh
+++ aqua_profile.plugin.zsh
@@ -1,11 +1,11 @@
-# The main file of the aqua_profile plugin
+# The main file of aqua`s profile plugin
 
 # check for Windows Subsystem for Linux
 if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop ]];
 then WSL=true; else WSL=false; fi
 
-# SETTINGS
-zstyle ':omz:update' mode auto
+
+# ENVIRONMENT
 export EDITOR='nvim';
 export BROWSER='firefox'
 [ $WSL = true ] && export OPENER='wsl-open'
@@ -14,18 +14,18 @@ export DISABLE_AUTO_TITLE='true'
 export NAP_DEFAULT_LANGUAGE='md'
 export ZELLIJ_CONFIG_DIR=/etc/zellij; export ZELLIJ_CONFIG_FILE=/etc/zellij/config.kdl
 # export GDK_DPI_SCALE=1.5
-bindkey ' ' magic-space
 
 # SYSTEM
+zstyle ':omz:update' mode auto
+bindkey ' ' magic-space
+
 alias q="exit"
 alias s="sudo"
 [ $WSL = true ] && alias open="wsl-open"
 
-# CLIPBOARD
+# clipboard
 alias yank="xclip -selection clipboard" && alias put="xclip -o -selection clipboard"
 [ $WSL = true ] && alias yank="wcopy" && alias put="wpaste" # use wsl-clipboard if on WSL
-
-# SYSTEM INFO
 alias ls="exa -l"
 alias lss="exa *"
 alias cls="clear && ls"
@@ -38,12 +38,10 @@ alias cat="bat"
 alias top="htop"
 
 # MOVEMENT
-alias ..='cd ../'
-alias ../='cd ../'
-alias ../../='cd ../../'
-alias ../..='cd ../../'
-alias ../../../='cd ../../../'
-alias ../../..='cd ../../../'
+alias h="cd ~"
+alias ..="cd ../"
+alias ...="cd ../../"
+alias ....="cd ../../../"
 
 # APPLICATIONS
 alias b="buku --suggest"
@@ -56,6 +54,7 @@ alias lg="lazygit"
 alias mail="aerc"
 alias p="python"
 alias py="python"
+alias c="code"
 alias v="nvim"
 alias vnim="nvim"
 
