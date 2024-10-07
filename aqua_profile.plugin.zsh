9a32c463ac8dbefdf7ec095376f1186b0cca6182
--global <--global>
2024-10-07 03:08:00 +0000

    vscode

--- aqua_profile.plugin.zsh
+++ aqua_profile.plugin.zsh
@@ -1,31 +1,34 @@
-# The main file of aqua`s profile plugin
+#!/bin/bash
+
+# The main file of the aqua_profile plugin
 
 # check for Windows Subsystem for Linux
-if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop ]];
+if [[ -f /proc/sys/fs/binfmt_misc/WSLInterop || "$TERM_PROGRAM" = 'vscode' ]]
 then WSL=true; else WSL=false; fi
 
-
-# ENVIRONMENT
+# SETTINGS
+zstyle ':omz:update' mode auto
 export EDITOR='nvim';
 export BROWSER='firefox'
+[ $WSL = true ] && export BROWSER='/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe'
 [ $WSL = true ] && export OPENER='wsl-open'
 
 export DISABLE_AUTO_TITLE='true'
 export NAP_DEFAULT_LANGUAGE='md'
 export ZELLIJ_CONFIG_DIR=/etc/zellij; export ZELLIJ_CONFIG_FILE=/etc/zellij/config.kdl
 # export GDK_DPI_SCALE=1.5
-
-# SYSTEM
-zstyle ':omz:update' mode auto
 bindkey ' ' magic-space
 
+# SYSTEM
 alias q="exit"
 alias s="sudo"
 [ $WSL = true ] && alias open="wsl-open"
 
-# clipboard
+# CLIPBOARD
 alias yank="xclip -selection clipboard" && alias put="xclip -o -selection clipboard"
 [ $WSL = true ] && alias yank="wcopy" && alias put="wpaste" # use wsl-clipboard if on WSL
+
+# SYSTEM INFO
 alias ls="exa -l"
 alias lss="exa *"
 alias cls="clear && ls"
@@ -38,10 +41,9 @@ alias cat="bat"
 alias top="htop"
 
 # MOVEMENT
-alias h="cd ~"
-alias ..="cd ../"
-alias ...="cd ../../"
-alias ....="cd ../../../"
+alias ..='cd ../'
+alias ...='cd ../../'
+alias ....='cd ../../../'
 
 # APPLICATIONS
 alias b="buku --suggest"
@@ -54,8 +56,9 @@ alias lg="lazygit"
 alias mail="aerc"
 alias p="python"
 alias py="python"
-alias c="code"
-alias v="nvim"
+
+alias e='"$EDITOR"'
+alias v='"$EDITOR"'
 alias vnim="nvim"
 
 alias zj="zellij"
@@ -67,5 +70,5 @@ alias zjlf="zjl"
 bindkey -s '^f' 'zellij run -- lf \n'
 
 # source the other aqua plugin files
-source ${0:A:h}/aqua_functions.zsh
-source ${0:A:h}/aqua_theme.zsh
+source "${0:A:h}"/aqua_functions.zsh
+source "${0:A:h}"/aqua_theme.zsh
