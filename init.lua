fbdc49f9496d406a0adcbae93bca5a3905168082
aqua <aqua.rock.slug@gmail.com>
2024-10-13 21:29:15 -0500

    now using mini config

--- init.lua
+++ init.lua
@@ -1,193 +1,92 @@
--- AQUA ARCH NEOVIM
--- The main nvim configuration file
-local vim = vim -- avoid undefined var warning
-
--- PLUGINS ---------------------------------------------------------------------
-local Plug = vim.fn["plug#"]
-vim.call("plug#begin", "/usr/share/nvim_plugged")
-
--- load mini plugins
-for _, plugname in pairs({
-	"animate",
-	"surround",
-	"ai",
-	"comment",
-	"icons",
-	"indentscope",
-	"jump",
-	"notify",
-	"pairs",
-	"splitjoin", -- gS
-	"trailspace",
-}) do
-	Plug("echasnovski/mini." .. plugname)
+local vim = vim
+local path_package = vim.fn.stdpath('data') .. '/site/'
+local mini_path = path_package .. 'pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+	vim.cmd('echo "Installing `mini.nvim`" | redraw')
+	local clone_cmd = { 'git', 'clone', '--filter=blob:none', 'https://github.com/echasnovski/mini.nvim', mini_path }
+	vim.fn.system(clone_cmd)
+	vim.cmd('packadd mini.nvim | helptags ALL')
+	vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
 
--- load other plugins
-Plug("OmniSharp/omnisharp-vim")
-Plug("simeji/winresizer")
-Plug("L3MON4D3/LuaSnip")
-Plug("Mofiqul/dracula.nvim", { as = "dracula" })
-Plug("MunifTanjim/nui.nvim")
-Plug("NvChad/nvim-colorizer.lua")
-Plug("akinsho/toggleterm.nvim")
-Plug("eoh-bse/minintro.nvim")
-Plug("errata-ai/vale")
-Plug("folke/todo-comments.nvim")
-Plug("hrsh7th/cmp-buffer")
-Plug("hrsh7th/cmp-cmdline")
-Plug("hrsh7th/cmp-nvim-lsp")
-Plug("hrsh7th/cmp-path")
-Plug("hrsh7th/nvim-cmp")
-Plug("luckasRanarison/nvim-devdocs")
-Plug("mfussenegger/nvim-lint")
-Plug("neovim/nvim-lspconfig")
-Plug("nvim-lua/plenary.nvim")
-Plug("nvim-lua/popup.nvim")
-Plug("nvim-lualine/lualine.nvim")
-Plug("nvim-telescope/telescope-file-browser.nvim")
-Plug("nvim-telescope/telescope.nvim")
-Plug("nvim-treesitter/nvim-treesitter")
-Plug("prichrd/netrw.nvim")
-Plug("rafamadriz/friendly-snippets")
-Plug("rinx/nvim-ripgrep")
-Plug("saadparwaiz1/cmp_luasnip")
-Plug("stevearc/conform.nvim")
-Plug("tree-sitter/tree-sitter")
-Plug("tree-sitter/tree-sitter-html")
-vim.call("plug#end")
+require('mini.deps').setup({ path = { package = path_package } })
+local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
--- SETUP ---------------------------------------------------------------------
-vim.g.mapleader = ","
-vim.opt.mousescroll = "ver:1" -- fixes scrolling with mini.animate
-vim.opt.termguicolors = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.autochdir = true
-vim.opt.scrolloff = 1000
+-- Safely execute immediately
+now(function()
+	vim.o.termguicolors = true
+	vim.g.mapleader = ","
+	vim.opt.mousescroll = "ver:1" -- fixes scrolling with mini.animate
+	vim.opt.termguicolors = true
+	vim.opt.autochdir = true
+	vim.opt.scrolloff = 1000
+end)
+now(function() require('mini.icons').setup() end)
+now(function() require('mini.tabline').setup() end)
+now(function() require('mini.statusline').setup() end)
+now(function() require('mini.starter').setup() end)
+now(function()
+	require('mini.notify').setup()
+	vim.notify = require('mini.notify').make_notify()
+end)
 
-require("mini.ai").setup()
-require("mini.animate").setup()
-require("mini.comment").setup()
-require("mini.indentscope").setup({ symbol = "󰈿" })
-require("mini.jump").setup()
-require("mini.notify").setup()
-require("mini.pairs").setup()
-require("mini.splitjoin").setup()
-require("mini.surround").setup()
-require("mini.trailspace").setup()
-require("mini.icons").setup()
+-- Safely execute later
+later(function() require('mini.ai').setup() end)
+later(function() require('mini.comment').setup() end)
+later(function() require('mini.pick').setup() end)
+later(function() require('mini.surround').setup() end)
+later(function() require('mini.pairs').setup() end)
+later(function() require('mini.trailspace').setup() end)
+later(function() require('mini.splitjoin').setup() end)
+later(function() require('mini.animate').setup() end)
+later(function() require("mini.indentscope").setup({ symbol = "󰈿" }) end)
 
-require("lualine").setup({ theme = "dracula-nvim" })
-require("minintro").setup({ color = "#af87ff" })
-require("netrw").setup()
-require("nvim-devdocs").setup()
-require("nvim-treesitter").setup({ auto_install = true })
-require("todo-comments").setup()
-require("toggleterm").setup({ shade_terminals = true })
-require("colorizer").setup()
--- require("winresizer").setup()
+-- opt plugins
+now(function() -- theme
+	add({ source = 'Mofiqul/dracula.nvim', as = 'dracula' })
+	require("dracula").setup({ italic_comment = true, transparent_bg = true })
+	vim.cmd([[colorscheme dracula]])
+end)
 
-require("dracula").setup({ italic_comment = true, transparent_bg = true })
-vim.cmd([[colorscheme dracula]])
+now(function()
+	add({ -- terminal
+		source = 'akinsho/toggleterm.nvim',
+	}) -- needs to load now because the config files reference it
+end)
 
-vim.api.nvim_create_user_command('StartupAnalyze', function()
-	vim.cmd([[/VIM STARTING/,$ sort! f /  [^ ]*  /]])
-end, {})
+now(function() -- lsp
+	add({
+		source = 'hrsh7th/cmp-nvim-lsp.git',
+		depends = { 'williamboman/mason.nvim', 'williamboman/mason-lspconfig.nvim',
+			'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp' },
+	})
+	require("mason").setup()
+	require("mason-lspconfig").setup({})
+	require('cmp').setup { sources = { { name = 'nvim_lsp' } } }
+	local c = require('cmp_nvim_lsp').default_capabilities()
+	require('lspconfig').lua_ls.setup { capabilities = c }
+	require('lspconfig').ast_grep.setup { capabilities = c }
+	require('lspconfig').basedpyright.setup { capabilities = c }
+	require('lspconfig').omnisharp.setup { capabilities = c }
+	require('lspconfig').bashls.setup { capabilities = c }
+end)
 
--- FORMATTER -------------------------------------------------------------------
-require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		python = { "isort", "black" },
-		javascript = { "prettier" },
-		typescript = { "prettier" },
-		sh = { "shellcheck", "shfmt" },
-		-- cs = { "omnisharp" },
-	},
-})
+later(function() -- file browsing
+	add({
+		source = 'prichrd/netrw.nvim',
+	})
+end)
 
--- SNIPPETS --------------------------------------------------------------------
-require("luasnip.loaders.from_vscode").load({
-	include = {
-		"csharp",
-		"html",
-		"javascript",
-		"lua",
-		"markdown",
-		"python",
-		"shell",
-		"typescript",
-	},
-})
-require("luasnip").filetype_extend("typescript", { "angular" })
+later(function() -- treesitter
+	add({
+		source = 'nvim-treesitter/nvim-treesitter',
+		checkout = 'master',
+		monitor = 'main',
+		hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+	})
+	require('nvim-treesitter.configs').setup({ highlight = { enable = true } })
+end)
 
--- LINTING ---------------------------------------------------------------------
-local lint = require("lint")
-lint.linters_by_ft = {
-	javascript = { "eslint" },
-	json = { "jsonlint" },
-	lua = { "luacheck" },
-	markdown = { "vale" },
-	python = { "pylint" },
-	-- cs = { "omnisharp" },
-}
-
--- LANGUAGE SERVERS ------------------------------------------------------------
-local lspconfig = require("lspconfig")
-lspconfig.pyright.setup({})
-lspconfig.ts_ls.setup({})
-lspconfig.omnisharp.setup({
-	cmd = { "/usr/lib/omnisharp-roslyn/OmniSharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
-})
-vim.api.nvim_create_autocmd(
-	{ "BufWritePost" }, -- lint and trim on save
-	{
-		callback = function()
-			lint.try_lint()
-			require("mini.trailspace").trim()
-		end,
-	}
-)
-
--- AUTO ----------------------------------------------------------------------
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
-
--- COMPLETION ------------------------------------------------------------------
-local cmp = require("cmp")
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-	}),
-	sources = cmp.config.sources({ { name = "nvim_lsp" }, { name = "luasnip" } }, { { name = "buffer" } }),
-})
-cmp.setup.cmdline({ "/", "?" }, {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = { { name = "buffer" } },
-})
-cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
-	matching = { disallow_symbol_nonprefix_matching = false },
-})
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-lspconfig["ts_ls"].setup({ capabilities = capabilities })
-lspconfig["pyright"].setup({ capabilities = capabilities })
-lspconfig["omnisharp"].setup({ capabilities = capabilities })
+-- AUTOCMD
+vim.api.nvim_create_autocmd("BufWritePost", { callback = require("mini.trailspace").trim })
+vim.api.nvim_create_autocmd("TextYankPost", { callback = vim.highlight.on_yank })
--- use_mini.sh
+++
@@ -1,4 +0,0 @@
-sudo cp ./mini_init.lua /etc/xdg/nvim/plugin/init.lua
-sudo cp ./mini_keymap.lua /etc/xdg/nvim/plugin/keymap.lua
-sudo rm -f /etc/xdg/nvim/plugin/telescope.lua
-
