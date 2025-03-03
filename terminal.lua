25f9864c0c3dcc037c971a5da06582269049dd1e
aquarock <aqua.rock.slug@gmail.com>
2025-03-03 17:42:00 -0600

    shell prompt

--- terminal.lua
+++ terminal.lua
@@ -1,10 +1,8 @@
 local tym = require("tym")
 
 tym.set("width", 100)
-tym.set("font", "FairiesevkaTerm Nerd Font Mono 16")
+tym.set("font", "FairiesevkaTerm Nerd Font Mono 18")
 tym.set_config({
 	shell = "/usr/bin/lush",
-	cursor_shape = "underline",
-	autohide = true,
 	color_foreground = "green",
 })
