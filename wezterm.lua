8549b550acedc2ada31b2bfcccf70ff403c17ba7
aquarock <aqua.rock.slug@gmail.com>
2025-04-13 20:43:51 -0500

    disabled wezterm tab bar

--- wezterm.lua
+++ wezterm.lua
@@ -14,12 +14,8 @@ config.font_size = 14
 config.default_cursor_style = 'BlinkingBar'
 
 config.window_background_opacity = 0.2
-config.use_fancy_tab_bar = false
-config.show_tabs_in_tab_bar = false
-config.show_new_tab_button_in_tab_bar = false
-
--- TODO
--- config.disable_default_key_bindings = true
--- config.launch_menu = launch_menu
+config.hide_tab_bar_if_only_one_tab = true
+config.enable_tab_bar = false
+-- config.tab_bar_at_bottom = true
 
 return config
