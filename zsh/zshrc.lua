762b727b8494e3b963744814ec6ad81722f2721c
aqua <aqua.rock.slug@gmail.com>
2025-06-30 09:54:34 -0500

    started using zshrc.lua

--- zsh/zshrc.lua
+++ zsh/zshrc.lua
@@ -1,4 +1,23 @@
 -- TODO use this file instead of zshrc
+-- generate and write .zshrc?
+-- run from /etc/zsh/zprofile before .zshrc is sourced
 
-local setenv = require("posix.stdlib").setenv
-setenv('SETENV', 'test')
+if os.getenv('AQUAMOON_STATE') == 'using_nvim' then os.exit() end
+
+local unistd = require("posix.unistd")
+local set = require("posix.stdlib").setenv
+
+set('AQUAMOON_STATE', 'using_nvim')
+os.execute('nvim +Oil')
+
+-- set('ZSH_THEME', 'dracula')
+-- set('TERM', 'wezterm')
+
+-- export PATH="$PATH:/home/aqua/.local/bin"
+-- export LUA_PATH="/home/aqua/.aquamoon/?.lua;;"
+-- export TERM="wezterm"
+-- export HISTFILE=$HOME/.zsh_history
+-- export HISTSIZE=10000
+-- export SAVEHIST=10000
+
+-- print(string.format('alias %s="%s"', "iv", "nvim"))
