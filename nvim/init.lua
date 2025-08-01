e40a454e95f274af6b931a0a837f178b2acb294f
aqua <aqua.rock.slug@gmail.com>
2025-08-01 17:18:54 -0500

    removed neovide padding

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,7 +37,7 @@ vim.g.neovide_opacity = 0.33
 vim.o.guifont = "BigBlueTermPlus Nerd Font Propo:h14"
 vim.g.neovide_text_gamma = 0.0
 vim.g.neovide_text_contrast = 0.5
-vim.g.neovide_padding_bottom = 15
+-- vim.g.neovide_padding_bottom = 15
 -- vim.g.neovide_cursor_vfx_mode = "torpedo"
 
 -- LANGUAGE SERVERS
