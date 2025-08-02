005973212a61163410cfb59a5a9c26dae828dcc0
aqua <aqua.rock.slug@gmail.com>
2025-08-02 00:48:48 -0500

    neovide 2

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,7 +38,7 @@ if vim.g.neovide then
 	vim.o.guifont = "BigBlueTermPlus Nerd Font Propo:h12"
 	vim.g.neovide_text_gamma = 0.0
 	vim.g.neovide_text_contrast = 0.5
-	-- vim.g.neovide_padding_bottom = 15
+	vim.g.neovide_padding_bottom = 1
 	-- vim.g.neovide_cursor_vfx_mode = "torpedo"
 
 	vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
