2a8c1c47af12d2be8de593063fc9f8de46e71a6b
aqua <aqua.rock.slug@gmail.com>
2025-08-02 02:40:40 -0500

    neovide 3

--- nvim/init.lua
+++ nvim/init.lua
@@ -41,13 +41,6 @@ if vim.g.neovide then
 	vim.g.neovide_padding_bottom = 1
 	-- vim.g.neovide_cursor_vfx_mode = "torpedo"
 
-	vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
-	vim.keymap.set('v', '<D-c>', '"+y')  -- Copy
-	vim.keymap.set('n', '<D-v>', '"+P')  -- Paste normal mode
-	vim.keymap.set('v', '<D-v>', '"+P')  -- Paste visual mode
-	vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
-	vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
-
 	-- change scale factor
 	vim.g.neovide_scale_factor = 1.0
 	local change_scale_factor = function(delta)
@@ -60,12 +53,6 @@ if vim.g.neovide then
 		change_scale_factor(1 / 1.25)
 	end)
 end
-
--- Allow clipboard copy paste in neovim
-vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
 
 -- LANGUAGE SERVERS
 require("lspconfig")["biome"].setup({})
