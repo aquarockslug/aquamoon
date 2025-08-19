b6dc5b56ee2134eae0d4cae017010ab006676303
aqua <aqua.rock.slug@gmail.com>
2025-08-19 09:33:05 -0500

    neovide padding

--- nvim/init.lua
+++ nvim/init.lua
@@ -42,6 +42,7 @@ if vim.g.neovide then
 	vim.g.neovide_text_gamma = 0.8
 	vim.g.neovide_text_contrast = 0.1
 	vim.g.neovide_padding_left = 10
+	vim.g.neovide_padding_top = 10
 	-- vim.g.neovide_cursor_vfx_mode = "torpedo"
 end
 
