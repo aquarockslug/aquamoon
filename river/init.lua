eac6d5c60e320667129319fd453259baf10ffdf9
aqua <aqua.rock.slug@gmail.com>
2025-08-20 02:58:54 -0500

    bug fix

--- river/init.lua
+++ river/init.lua
@@ -1,14 +1,17 @@
 R = require 'river'
-local createSettings = require 'settings'
+S = require 'settings'
+local themes = require 'settings/theme'
 
--- TODO local set_by_clock = theme_list[math.ceil(tonumber(os.date("%H")) / 24 * 3)]
-local theme = require("settings/theme").get("sweetie")
-
-settings = createSettings(theme)
+if arg[1] ~= nil then
+	S.theme = themes.get(arg[1])
+else
+	-- TODO local set_by_clock = theme_list[math.ceil(tonumber(os.date("%H")) / 24 * 3)]
+	S.theme = themes.get("sweetie")
+end
 
 -- local drun = require 'etc/drun'
 -- drun(S.theme.name)
 
-R.apply_settings(settings)
+R.apply_settings(S)
 
 R.run()
