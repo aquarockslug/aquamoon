0b7605648a0679a7668c1329ceabbcf83f6b5e13
aqua <aqua.rock.slug@gmail.com>
2025-08-20 04:26:18 -0500

    idea comments

--- river/init.lua
+++ river/init.lua
@@ -9,7 +9,8 @@ end
 
 -- TODO theme_name -> settings
 
--- write the active theme to a file so that nvim and tofi can use it?
+-- TODO send active theme to tofi by key mapping it with send-layout-cmd
+-- write the active theme to a file so that nvim and tofi can read it?
 -- pass settings through run and save it in the layout?
 
 R.apply_settings(S)
