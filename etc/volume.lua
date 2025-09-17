09442b72e2fe9a81293457bdf517971f45b695fc
aqua <aqua.rock.slug@gmail.com>
2025-09-16 22:01:53 -0500

    created volume.lua

---
+++ etc/volume.lua
@@ -0,0 +1,21 @@
+-- TODO
+local function get_volume(audio_sink)
+	return 0.01
+end
+
+local function adjust(audio_sink, amount)
+	local curr_volume = get_volume(audio_sink)
+	local new_volume = curr_volume + (amount / 1000)
+	print(curr_volume, new_volume)
+
+	-- os.execute("pactl set-sink-volume " .. str(audio_sink) .. " " .. str(new_volume))
+
+	os.execute("echo " .. new_volume .. " > /tmp/wobpipe")
+end
+
+local sink_number = 1
+
+M = {}
+M.raise = function(amount) adjust(sink_number, amount) end
+M.lower = function(amount) adjust(sink_number, -1 * amount) end
+return M
