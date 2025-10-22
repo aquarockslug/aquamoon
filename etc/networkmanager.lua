66b75ccb9c1e40eea94e062e4b966644a1d09313
aqua <aquarockslug@gmail.com>
2025-10-22 04:42:35 -0500

    networkmanager dmenu broken theme

--- etc/networkmanager.lua
+++ etc/networkmanager.lua
@@ -2,12 +2,11 @@ package.path = '/home/aqua/.aquamoon/?.lua;/home/aqua/.aquamoon/?/?.lua;' ..
     '/home/aqua/.aquamoon/rocks/share/lua/5.1/?.lua;' ..
     '/home/aqua/.aquamoon/rocks/share/lua/5.1/?/?.lua;;'
 
-S = require "settings"
 cmd = "networkmanager_dmenu"
-default_style = S.theme.tofi_style
-for i, arg in ipairs(default_style) do
-	cmd = cmd .. " " .. arg
-end
+
+-- TODO set up tofi menu
+-- local tofi_style = require("settings").theme.tofi
+-- local menu = require("etc/tofi").options(tofi_style)
 
 -- override the width so this menu is wider than other menus
 cmd = cmd .. " --width=50%"
