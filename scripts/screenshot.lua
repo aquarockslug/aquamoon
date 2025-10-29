42675656708cc20746861a2bd67737ca39a8b40a
aqua <aquarockslug@gmail.com>
2025-10-29 00:36:07 -0500

    screenshot names

--- scripts/screenshot.lua
+++ scripts/screenshot.lua
@@ -1,11 +1,17 @@
-local path = "~/Pictures/screenshot.jpg"
+-- take a screenshot and open it with loupe image viewer
+
+local file_name = "screenshot_" .. os.date("_at_%I_%M%p")
+local path = "~/Pictures/Screenshots/" .. file_name .. ".jpg"
 
 -- Take the screenshot and save it to "path"
 os.execute("grim -t jpeg " .. path)
 
--- Quickly open the image for cropping
+-- Copy the image to the system clipboard
+os.execute("cat " .. path .. " | wl-copy")
+
+-- Open the image for cropping
 os.execute("riverctl spawn 'loupe " .. path .. "'")
--- os.execute("gimp --no-splash --no-data " .. path .. " &")
 
 -- Notify
-os.execute("notify-send '󰈿 Screenshot Taken'")
+-- TODO add an action to the notification that opens loupe or gimp
+os.execute("dunstify '󰈿 Screenshot Taken'")
