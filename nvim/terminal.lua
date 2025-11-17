26505b79492029b58928c58412eb0616403df981
aqua <aquarockslug@gmail.com>
2025-11-17 02:44:02 -0600

    rocks bootstrap

--- nvim/terminal.lua
+++ nvim/terminal.lua
@@ -1,25 +1,27 @@
 -- AQUAMOON file for configuring nvim's terminal
 
 -- function for setting environment variables in the nvim terminal
-function Let(name, value) vim.cmd("let $" .. name .. " = \'" .. value .. "\'") end
+function Let(name, value)
+	vim.cmd("let $" .. string.upper(name) .. " = \'" .. value .. "\'")
+end
 
-Let("HISTSIZE", "10000")
-Let("SAVEHIST", "10000")
+Let("histsize", "10000")
+Let("savehist", "10000")
 
 local theme = require "settings".theme
-Let("PS1", "%F{#" .. theme.fg .. "}%1~%k 󰈿 %f")
-Let("BAT_THEME", "fly16")
-Let("PAGER", "bat --theme fly16 --style=-numbers") -- alternatives: more, nvim
-Let("DDGR_COLORS", "fHgffH")
+Let("ps1", "%F{#" .. theme.fg .. "}%1~%k 󰈿 %f")
+Let("bat_theme", "fly16")
+Let("pager", "bat --theme fly16 --style=-numbers") -- alternatives: more, nvim
+Let("ddgr_colors", "fHgffH")
 
 local rocks_path = os.getenv("HOME") .. "/.local/share/nvim/rocks/share/lua/5.1/?.lua;"
 rocks_path = rocks_path .. os.getenv("HOME") .. "/.local/share/nvim/rocks/share/lua/5.1/?/init.lua;"
 local rocks_cpath = os.getenv("HOME") .. "/.local/share/nvim/rocks/lib/lua/5.1/?.so;"
 rocks_cpath = rocks_cpath .. os.getenv("HOME") .. "/.local/share/nvim/rocks/lib64/lua/5.1/?.so;"
 local aquamoon_path = os.getenv("HOME") .. "/.aquamoon/?.lua;" .. os.getenv("HOME") .. "/.aquamoon/?/?.lua;"
-Let("LUA_PATH", rocks_path .. aquamoon_path .. ";")
-Let("LUA_CPATH", rocks_cpath .. ";")
+Let("lua_path", rocks_path .. aquamoon_path .. ";")
+Let("lua_cpath", rocks_cpath .. ";")
 
-Let("PATH", os.getenv("PATH") .. ":/home/aqua/.local/bin")
+Let("path", os.getenv("PATH") .. ":/home/aqua/.local/bin")
 
 -- TODO put aliases here?
