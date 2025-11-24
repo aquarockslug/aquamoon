2c2d9b2875fb1fef5e770e467037d758b8f3ccd6
aqualava <aqua.rock.slug@gmail.com>
2025-11-24 07:37:43 -0600

    paths fix

--- install.lua
+++ install.lua
@@ -118,14 +118,14 @@ if exec("nvim -u NORC -c \"source https://raw.githubusercontent.com/lumen-oss/ro
 end
 
 -- create directories and copy paths.lua
-print_section("Setting up Lua paths")
-if exec("sudo mkdir -p /usr/share/lua/5.4 /usr/share/luajit") then
-    print_success("Created Lua directories")
-end
-if exec("sudo cp paths.lua /usr/share/lua/5.4/") then
+-- print_section("Setting up Lua paths")
+-- if exec("sudo mkdir -p /usr/share/lua/5.4 /usr/share/luajit-2.1") then
+--     print_success("Created Lua directories")
+-- end
+if exec("sudo cp paths.lua /usr/share/lua/5.4/lib") then
     print_success("Copied paths.lua to Lua 5.4")
 end
-if exec("sudo cp paths.lua /usr/share/luajit-2.1/") then
+if exec("sudo cp paths.lua /usr/share/luajit-2.1/lib") then
     print_success("Copied paths.lua to LuaJIT")
 end
 
