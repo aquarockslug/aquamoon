060b7a44e9e2bd372842b7e3c4531ceb76874a39
aqualava <aqua.rock.slug@gmail.com>
2025-11-23 20:25:57 -0600

    reorg nvim init

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,6 +21,51 @@ vim.g.oceanic_next_terminal_italic = 1
 vim.flag = "󰈿"
 
 
+-- PLUGINS
+require "nvim/rocks"
+require "neomodern".setup({ theme = "iceclimber", code_style = { comments = "italic" } })
+require "leap".setup({})
+require "debugprint".setup({
+	-- keymaps = { normal = { plain_below = "<leader>v", plain_above = "<leader>V" } }
+})
+require("tv").setup({
+	keybindings = nvim_mappings.tv,
+	window = {
+		width = 1.0,
+		height = 1.0,
+	}
+})
+require("snipe").setup({
+	ui = {
+		position = "center",
+		text_align = "file-first",
+		open_win_override = {
+			title = vim.flag,
+			border = "rounded"
+		}
+	},
+	navigate = { open_vsplit = "e", open_split = "E" }
+})
+
+-- NEOVIDE
+if vim.g.neovide then
+	vim.g.neovide_opacity = S.theme.opacity
+	vim.o.guifont = S.theme.active_font.name
+	vim.g.neovide_text_gamma = 0.8
+	vim.g.neovide_text_contrast = 0.1
+	vim.g.neovide_padding_left = 10
+	vim.g.neovide_padding_top = 10
+	vim.opt.linespace = 3
+
+	if S.theme_name == "OceanicNext" or S.theme_name == "minicyan" then
+		vim.g.neovide_cursor_vfx_mode = "torpedo"
+	end
+	if S.theme_name == "srcery" or S.theme_name == "eldritch" then
+		vim.g.neovide_cursor_vfx_mode = "pixiedust"
+	end
+end
+
+
 -- LANGUAGE SERVERS
 local lspconfig = require('lspconfig')
 lspconfig.biome.setup({})
@@ -39,7 +84,7 @@ vim.diagnostic.enable(false)
 
 -- SAVE
 save = function()
-	-- prevent oil warning
+	-- avoid warnings from oil and ministart filetype
 	if vim.bo.filetype ~= "oil" and vim.bo.filetype ~= "ministarter" then
 		MiniTrailspace.trim()
 		vim.lsp.buf.format()
