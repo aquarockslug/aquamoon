a220a0b2d05d9005e3f3dd52f179f8a750d82860
aqualava <aqua.rock.slug@gmail.com>
2025-12-06 05:35:55 -0600

    networkmanager

--- scripts/networkmanager.lua
+++ scripts/networkmanager.lua
@@ -2,13 +2,12 @@ package.path = '/home/aqua/.aquamoon/?.lua;/home/aqua/.aquamoon/?/?.lua;' ..
     '/home/aqua/.aquamoon/rocks/share/lua/5.1/?.lua;' ..
     '/home/aqua/.aquamoon/rocks/share/lua/5.1/?/?.lua;;'
 
-cmd = "networkmanager_dmenu"
+-- get tofi style from theme
+local tofi_style = require("settings").theme.tofi
 
--- TODO set up tofi menu
--- local tofi_style = require("settings").theme.tofi
--- local menu = require("etc/tofi").options(tofi_style)
-
--- override the width so this menu is wider than other menus
-cmd = cmd .. " --width=50%"
+local cmd = "networkmanager_dmenu"
+for setting, value in pairs(tofi_style) do
+	cmd = cmd .. " --" .. setting .. "=" .. value
+end
 
 os.execute(cmd)
