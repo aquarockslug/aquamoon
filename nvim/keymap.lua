06d2535de701259e64198d8fa334b3d1109a1ee0
aqualava <aqua.rock.slug@gmail.com>
2025-12-19 17:05:07 -0600

    keymap refactor

--- nvim/keymap.lua
+++ nvim/keymap.lua
@@ -1,78 +1,111 @@
--- KEYMAP
+-- KEYMAP CONFIGURATION
 local vim = vim
 
-vim.keymap.set({ "n", "x", "o" }, "<CR>", "<Plug>(leap)")
+-- Utility functions
+local M = {}
 
-vim.keymap.set("n", "U", "<c-r>")        -- redo
-
-vim.cmd.tnoremap("<Esc>", "<C-\\><C-n>") -- exit terminal with Esc
-
-vim.cmd.toggle_diagnostics = function()
+function M.toggle_diagnostics()
 	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
 end
 
-require('smart-splits').setup()
-vim.keymap.set('n', '<A-Left>', require('smart-splits').resize_left)
-vim.keymap.set('n', '<A-Down>', require('smart-splits').resize_down)
-vim.keymap.set('n', '<A-Up>', require('smart-splits').resize_up)
-vim.keymap.set('n', '<A-Right>', require('smart-splits').resize_right)
-vim.keymap.set('n', '<Left>', require('smart-splits').move_cursor_left)
-vim.keymap.set('n', '<Down>', require('smart-splits').move_cursor_down)
-vim.keymap.set('n', '<Up>', require('smart-splits').move_cursor_up)
-vim.keymap.set('n', '<Right>', require('smart-splits').move_cursor_right)
-vim.keymap.set('n', '<C-\\>', require('smart-splits').move_cursor_previous)
-vim.keymap.set('n', '<leader><Left>', require('smart-splits').swap_buf_left)
-vim.keymap.set('n', '<leader><Down>', require('smart-splits').swap_buf_down)
-vim.keymap.set('n', '<leader><Up>', require('smart-splits').swap_buf_up)
-vim.keymap.set('n', '<leader><Right>', require('smart-splits').swap_buf_right)
+function M.show_cursor_position()
+	local cursor_row, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
+	require("fidget").notify(
+		"Row: " .. tostring(cursor_row) .. ", " ..
+		"Col: " .. tostring(cursor_col)
+	)
+end
 
-for key, func in pairs({
-	-- left hand top row
-	r = function() vim.cmd "terminal scooter" end,
+function M.adjust_neovide_scale(delta)
+	vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + delta
+end
+
+function M.open_terminal(command)
+	vim.cmd("terminal " .. command)
+end
+
+-- Core keymaps
+vim.keymap.set({ "n", "x", "o" }, "<CR>", "<Plug>(leap)")
+vim.keymap.set("n", "U", "<c-r>")
+vim.cmd.tnoremap("<Esc>", "<C-\\><C-n>")
+
+-- Smart splits configuration
+local smart_splits = require('smart-splits')
+local split_keymaps = {
+	-- Window resizing
+	['<A-Left>'] = smart_splits.resize_left,
+	['<A-Down>'] = smart_splits.resize_down,
+	['<A-Up>'] = smart_splits.resize_up,
+	['<A-Right>'] = smart_splits.resize_right,
+
+	-- Cursor movement between windows
+	['<Left>'] = smart_splits.move_cursor_left,
+	['<Down>'] = smart_splits.move_cursor_down,
+	['<Up>'] = smart_splits.move_cursor_up,
+	['<Right>'] = smart_splits.move_cursor_right,
+	['<C-\\>'] = smart_splits.move_cursor_previous,
+
+	-- Buffer swapping
+	['<leader><Left>'] = smart_splits.swap_buf_left,
+	['<leader><Down>'] = smart_splits.swap_buf_down,
+	['<leader><Up>'] = smart_splits.swap_buf_up,
+	['<leader><Right>'] = smart_splits.swap_buf_right,
+}
+
+for key, func in pairs(split_keymaps) do
+	vim.keymap.set('n', key, func)
+end
+
+-- Leader keymaps
+local leader_keymaps = {
+	-- Left hand top row
+	r = function() M.open_terminal("scooter") end,
 	e = vim.cmd.Oil,
 	w = vim.cmd.terminal,
 	q = vim.cmd.bd,
 
-	-- left hand home row
-	d = vim.cmd.toggle_diagnostics,
+	-- Left hand home row
+	d = M.toggle_diagnostics,
 
-	-- left hand lower
-	c = function()
-		local cursor_row, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
-		require("fidget").notify(
-			"Row: " .. tostring(cursor_row) .. ", " ..
-			"Col: " .. tostring(cursor_col)
-		)
-	end,
+	-- Left hand lower
+	c = M.show_cursor_position,
 
-	-- right hand top
-	y = function() vim.cmd "terminal clipse" end,
+	-- Right hand top
+	y = function() M.open_terminal("clipse") end,
 	i = vim.lsp.buf.hover,
-	o = function() vim.cmd "terminal opencode" end,
+	o = function() M.open_terminal("opencode") end,
 
-	-- right hand center
+	-- Right hand center
 	h = function() vim.cmd "LazyGitFilterCurrentFile" end,
-	j = function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1 end,
-	k = function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1 end,
+	j = function() M.adjust_neovide_scale(-0.1) end,
+	k = function() M.adjust_neovide_scale(0.1) end,
 
-	-- right hand bottom
-	n = function() vim.cmd "terminal wiremix" end,
-	["/"] = vim.cmd.noh
-}) do
+	-- Right hand bottom
+	n = function() M.open_terminal("wiremix") end,
+	["/"] = vim.cmd.noh,
+}
+
+for key, func in pairs(leader_keymaps) do
 	vim.keymap.set({ "n", "x", "o" }, "<leader>" .. key, func)
 end
 
-for cmd, func in pairs({
-	-- right hand
+-- Function key keymaps
+local function_keymaps = {
+	-- Right hand (F1-F4)
 	[1] = function() vim.cmd "LazyGit" end,
-	[2] = vim.cmd.aqua_save, -- from init.lua
+	[2] = vim.cmd.aqua_save,
 	[3] = function() vim.cmd.split "./" end,
 	[4] = function() vim.cmd.vsplit "./" end,
-	-- left hand
+
+	-- Left hand (F5-F8)
 	[5] = vim.cmd.cprev,
 	[6] = vim.cmd.cnext,
 	[7] = function() require("snipe").open_buffer_menu() end,
-	[8] = vim.cmd.aqua_run, -- from init.lua
-}) do
+	[8] = vim.cmd.aqua_run,
+}
+
+for cmd, func in pairs(function_keymaps) do
 	vim.keymap.set({ "n", "i" }, "<F" .. cmd .. ">", func)
 end
+
+return M
