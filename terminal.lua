bd4ee41921bf3f60e5a8da7ba27770c5a6bfc3e6
aqua <aqua.rock.slug@gmail.com>
2026-02-20 21:35:11 -0600

    clean

--- terminal.lua
+++ terminal.lua
@@ -1,7 +1,5 @@
 local hilbish = require 'hilbish'
 local lunacolors = require 'lunacolors'
-local bait = require 'bait'
-local ansikit = require 'ansikit'
 
 hilbish.opts = {
 	autocd = true,
@@ -12,47 +10,17 @@ hilbish.opts = {
 	notifyJobFinish = true,
 }
 
-local unreadCount = 0
-local running = false
-local function doPrompt(fail)
-	hilbish.prompt(lunacolors.format(
-		'{blue}%u {cyan}%d ' .. (fail and '{red}' or '{green}') .. '󰈿 '
-	))
-end
+hilbish.prompt(lunacolors.format(
+	'{blue}%u {cyan}%d ' .. (fail and '{red}' or '{green}') .. '󰈿 '
+))
 
-local function doNotifyPrompt()
-	if running or unreadCount == hilbish.messages.unreadCount() then return end
+local ls = "eza --color --icons -F --hyperlink "
+os.execute(ls .. hilbish.cwd())
 
-	local notifPrompt = string.format('• %s unread notification%s', hilbish.messages.unreadCount(),
-		hilbish.messages.unreadCount() > 1 and 's' or '')
-	unreadCount = hilbish.messages.unreadCount()
-	hilbish.prompt(lunacolors.blue(notifPrompt), 'right')
-
-	hilbish.timeout(function()
-		hilbish.prompt('', 'right')
-	end, 3000)
-end
-
-doPrompt()
-
-bait.catch('command.preexec', function()
-	running = true
-end)
-
-bait.catch('command.exit', function(code)
-	running = false
-	doPrompt(code ~= 0)
-	doNotifyPrompt()
-end)
-
-bait.catch('hilbish.vimMode', function(mode)
-	if mode ~= 'insert' then
-		ansikit.cursorStyle(ansikit.blockCursor)
-	else
-		ansikit.cursorStyle(ansikit.lineCursor)
-	end
-end)
-
-bait.catch('hilbish.notification', function(notif)
-	doNotifyPrompt()
-end)
+hilbish.alias("df", "duf")
+hilbish.alias("du", "dust")
+hilbish.alias("q", "exit")
+hilbish.alias("l", "clear && " .. ls)
+hilbish.alias("lg", "lazygit")
+hilbish.alias("paru", "paru --bottomup")
+hilbish.alias("chmodx", "sudo chmod u+x")
