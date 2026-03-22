ba857c799b6459f0e73c47de827316cd47e711b9
aqua <aqua.rock.slug@gmail.com>
2026-03-22 18:12:48 -0500

    input update

--- river/init.lua
+++ river/init.lua
@@ -3,15 +3,12 @@ package.path = "" ..
     os.getenv("HOME") .. "/.aquamoon/?.lua;" ..
     os.getenv("HOME") .. "/.aquamoon/?/?.lua;;"
 
-os.execute("riverctl input pointer-1739-32183-SYNA7DB5:01_06CB:7DB7_Touchpad tap enabled")
+os.execute("riverctl input pointer-1149-8264-Kensington_Eagle_Trackball pointer-accel 4")
+os.execute("pointer-2362-8203-PIXA200B:00_093A:200B_Touchpad pointer-accel 4")
 
 R = require "river"
 S = require "settings"
 
 R.apply_settings(S)
-
--- if S.theme_name == "dracula" then
--- 	os.execute("fish_config theme choose dracula")
--- end
 
 require("scripts/write_configs").update_all(S.theme_name)
