d472a45ec62cf6446a070cca5abd260fced7fb9e
aqua <aqua.rock.slug@gmail.com>
2026-07-10 06:47:09 -0500

    more master menu entries

--- scripts/menu/master.lua
+++ scripts/menu/master.lua
@@ -8,30 +8,41 @@ local date = io.popen("date '+%I:%M%P on %A, %B %d'"):read("*a")
 local battery = io.popen("cat /sys/class/power_supply/BAT0/capacity"):read("*a")
 local font = (S.theme.active_font.name:match("^([^:]+)") or S.theme.active_font.name)
 
+-- Run a lua script from the scripts directory
 local script = function(script_path)
 	return "lua " .. S.path .. "/scripts/" .. script_path .. ".lua"
 end
 
+-- Open a new neovide window and run the given nvim command
+local neovide = function(cmd)
+	return [[riverctl spawn "neovide +']] .. cmd .. [['"]]
+end
+
+-- TODO configure this in a toml script?
 local items = {
 	date,
 	"Battery: " .. battery,
 	{ name = "Theme: " .. S.theme.name, value = script("theme/theme_picker") },
 	{ name = "Font: " .. font,          value = script("theme/theme_picker") },
 	"--------------------------------------",
-	{ name = "Web Search",  value = script("menu/browse") },
-	{ name = "Bookmarks",   value = script("menu/bookmarks") },
-	{ name = "Clipboard",   value = script("menu/clipboard") },
-	{ name = "Display",     value = script("menu/display") },
-	{ name = "Networks",    value = script("menu/networkmanager") },
-	{ name = "Screenshot",  value = script("sys/screenshot") },
-	{ name = "Eyedropper",  value = script("sys/pick_color") },
-	{ name = "Screensaver", value = script("daemon/screensaver") },
-	{ name = "Bluetooth",   value = "riverctl spawn 'neovide term://bluetui'" },
-	{ name = "Git",         value = "riverctl spawn 'neovide +LazyGit'" },
-	{ name = "Suspend",     value = "systemctl suspend" },
-	{ name = "Reboot",      value = "systemctl reboot" },
-	{ name = "Poweroff",    value = "systemctl poweroff" },
-	{ name = "Logout",      value = "riverctl exit" },
+	{ name = "Web Search",    value = script("menu/browse") },
+	{ name = "Bookmarks",     value = script("menu/bookmarks") },
+	{ name = "Clipboard",     value = script("menu/clipboard") },
+	{ name = "Display",       value = script("menu/display") },
+	{ name = "Network",       value = script("menu/networkmanager") },
+	{ name = "Screenshot",    value = script("sys/screenshot") },
+	{ name = "Eyedropper",    value = script("sys/pick_color") },
+	{ name = "System Status", value = script("river/status") },
+	{ name = "Screensaver",   value = script("daemon/screensaver") },
+	{ name = "Bluetooth",     value = neovide("Term bluetui") },
+	{ name = "Packages",      value = neovide("Tv pacman-packages") }, -- run tv picker
+	{ name = "Git",           value = neovide("LazyGit") },
+	{ name = "OpenCode",      value = neovide("tab AI") },      -- run scripts defined by cling.nvim
+	{ name = "Audio",         value = "pavucontrol" },          -- TODO replace audio settings interface
+	{ name = "Suspend",       value = "systemctl suspend" },
+	{ name = "Reboot",        value = "systemctl reboot" },
+	{ name = "Poweroff",      value = "systemctl poweroff" },
+	{ name = "Logout",        value = "riverctl exit" },
 }
 
 local selection = tofi.choices(items).open()
